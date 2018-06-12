4c27aee04f72018e987aabab8a96f274539a8264
Marty Pitt <martypitt@gmail.com>
2018-06-12 14:25:34 +0100

    fixed setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 chmod +x jailbreak.py
 rm /usr/local/bin/jailbreak 2> /dev/null
-ln -s $(pwd)/jailbreak.py /usr/local/bin/tino
+ln -s $(pwd)/jailbreak.py /usr/local/bin/jailbreak
